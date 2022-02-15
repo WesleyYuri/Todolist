ffa28f35e1ca1c48371910b99ae507c470a0c443
vagrant <vagrant@homestead.test>
2022-02-15 20:26:19 +0000

    Add new todo

--- app/Http/Livewire/Todolist.php
+++ app/Http/Livewire/Todolist.php
@@ -6,6 +6,24 @@ use Livewire\Component;
 
 class Todolist extends Component
 {
+    public $newTodo;
+
+    public $todolist = [
+        [
+            "todo" => "task 1"
+        ]
+    ];
+
+    public function addTodo(){
+        if($this->newTodo == ""){
+            return;
+        }
+        array_unshift($this->todolist, [
+            "todo" => $this->newTodo
+        ]);
+        $this->newTodo = "";
+    }
+
     public function render()
     {
         return view('livewire.todolist');
--- resources/views/livewire/todolist.blade.php
+++ resources/views/livewire/todolist.blade.php
@@ -1,12 +1,12 @@
 <div class="flex justify-center">
     <div class="card rounded my-10 w-6/12">
         <h1 class="mt-10 text-3xl">To-do List</h1>
-        <div class="mt-4 flex">
-            <input type="text" class="w-full rounded border shadow p-2 mr-2 my-2" placeholder="Add your new todo." />
+        <form class="mt-4 flex" wire:submit.prevent="addTodo">
+            <input type="text" class="w-full rounded border shadow p-2 mr-2 my-2" placeholder="Add your new todo." wire:model.lazy="newTodo" />
             <div class="py-2">
-                <button class="bg-blue-500 w-20 rounded shadow text-white p-2">+</button>
+                <button type="submit" class="bg-blue-500 w-20 rounded shadow text-white p-2">+</button>
             </div>
-        </div>
+        </form>
         <div class="mt-10">
             <table class="w-full divide-y divide-gray-200">
                 <thead>
@@ -18,24 +18,14 @@
                     </tr>
                 </thead>
                 <tbody class="divide-y divide-gray-200">
-                    <tr>
-                        <td class="text-center"><input type="checkbox"/></td>
-                        <td class="text-center">Task 1</td>
-                        <td class="text-center"><button class="bg-amber-500 w-20 rounded shadow text-white p-2">Edit</button></td>
-                        <td class="text-center"><button class="bg-red-500 w-20 rounded shadow text-white p-2">X</button></td>
-                    </tr>
-                    <tr>
-                        <td class="text-center"><input type="checkbox"/></td>
-                        <td class="text-center">Task 2</td>
-                        <td class="text-center"><button class="bg-amber-500 w-20 rounded shadow text-white p-2">Edit</button></td>
-                        <td class="text-center"><button class="bg-red-500 w-20 rounded shadow text-white p-2">X</button></td>
-                    </tr>
-                    <tr>
-                        <td class="text-center"><input type="checkbox"/></td>
-                        <td class="text-center">Task 3</td>
-                        <td class="text-center"><button class="bg-amber-500 w-20 rounded shadow text-white p-2">Edit</button></td>
-                        <td class="text-center"><button class="bg-red-500 w-20 rounded shadow text-white p-2">X</button></td>
-                    </tr>
+                    @foreach($todolist as $todo)
+                        <tr>
+                            <td class="text-center"><input type="checkbox"/></td>
+                            <td class="text-center">{{ $todo['todo'] }}</td>
+                            <td class="text-center"><button class="bg-amber-500 w-20 rounded shadow text-white p-2">Edit</button></td>
+                            <td class="text-center"><button class="bg-red-500 w-20 rounded shadow text-white p-2">X</button></td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
